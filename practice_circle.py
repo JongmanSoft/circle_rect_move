3e5c968ea8eeb93ba70f6f8ea157cd2072317885
코딩천재이종만 <jongman@tukorea.ac.kr>
2023-09-20 18:25:15 +0900

    급하게 옮겨쓰기...
    춥다... 글씨도 안보인다...ㅠㅠ

---
+++ practice_circle.py
@@ -0,0 +1,38 @@
+import math
+from pico2d import *
+
+open_canvas()
+
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+def run_circle():
+    print("circle")
+
+    cx, cy, r = 800 / 2, 600 / 2, 200
+    for deg in range(0,360,5):
+        x = cx + r * math.cos(deg/360 * 2 * math.pi)
+        y = cy + r * math.sin(deg/360 * 2 * math.pi)
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,y)
+        delay(0.1)
+
+def run_rectangle():
+    print("rectangle")
+
+    for i in range (750 , 50-1, -10):
+        x = i
+        y = 540
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        delay(0.1)
+       
+    pass
+
+while True:
+    run_circle()
+    run_rectangle()
+    break
+close_canvas()
